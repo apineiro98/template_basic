2d93c6ae8590749d7dc5177f4a587749d19dbc5a
apineiro98 <alejopineiro98@gmail.com>
2023-08-31 14:09:41 -0300

    Finish

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 //! Ham Menu **//
 ((d) => {
   const $barButton = d.querySelector('.menu-btn'),
@@ -21,6 +20,40 @@
     $barButton.lastElementChild.classList.add('none');
     $menu.classList.remove('is-active');
 
+  });
+
+})(document);
+
+//! Contact-Form **//
+((d) => {
+  const $form = d.querySelector(".contact-form"),
+    $loader = d.querySelector(".contact-form-loader"),
+    $response = d.querySelector(".contact-form-response")
+  ;
+
+  $form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    $loader.classList.remove("none");
+    fetch("https://formsubmit.co/ajax/alejopineiro98@gmail.com", {
+      method: "POST",
+      body: new FormData(e.target),
+    })
+      .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+      .then(json => {
+        console.log(json);
+        location.hash = "#gracias";
+        $form.reset();
+      })
+      .catch(err => {
+        console.log(err);
+        let message = err.statusText || "Ocurrio un error al enviar, intenta nuevamente!";
+        $response.querySelector("h3").innerHTML = `Error ${err.status}: ${message}`;
+      }).finally(() => {
+        $loader.classList.add("none");
+        setTimeout(() => {
+          location.hash = "#close";
+        }, 3000);
+      });
   });
 
 })(document);
